6593415282890a5eed538a453232c5eaf0ed09c3
hanker <idlehanker@gmail.com>
2017-08-22 10:37:08 +0800

    add groovy

---
+++ groovy/ReadME.mk
@@ -0,0 +1,8 @@
+# Basic
+groovy automatically imports the packages:
++ `groovy.lang.*`
++ `groovy.util.*`
++ `java.util.*`
++ `java.net.*`
++ `java.io.*` as well as the classes
++  `java.math.BigInteger` and `java.math.BigDecimal`
